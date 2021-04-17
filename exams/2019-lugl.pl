5f34f909ced2f966b48010d3f6de0fc83d0b067a
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-04-17 16:45:58 +0200

    add exam 2019-lugl

---
+++ exams/2019-lugl.pl
@@ -0,0 +1,14 @@
+% swipl
+% consult('exams/2019-lugl.pl').
+
+% disordinati(+L,?D)
+% ha successo se e solo se L è una lista di interi e D è una lista che contiene gli elementi di L che sono minori dell'elemento precedente.
+% Gli elementi di D devono occorrere nello stesso ordine in cui occorrono in L. Ovviamente, se L è ordinata, D sarà vuota.
+
+% example
+% disordinati([1,10,5,6,7,4,9],Result)
+
+disordinati([],[]).
+disordinati([_],[]).
+disordinati([X,Y|Rest],[Y|Result]) :- Y < X, disordinati([Y|Rest],Result).
+disordinati([_,Y|Rest],Result) :- disordinati([Y|Rest],Result).
