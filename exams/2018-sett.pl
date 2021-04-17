74c665725df65964e3402ed7419a1d84b98d8ece
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-04-17 16:06:04 +0200

    update exam 2018-sett

--- exams/2018-sett.pl
+++ exams/2018-sett.pl
@@ -4,9 +4,9 @@
 % foglie(+Tree,-Foglie)
 % ha successo quando Tree è un albero binario e Foglie è una lista delle foglie di Tree (in qualsiasi ordine ed eventualmente con ripetizioni).
 
-foglie(empty, []).
-foglie(t(X, empty, empty), [X]) :- !.
-foglie(t(_, T_sx, T_dx), F) :- foglie(T_sx, F_sx), foglie(T_dx, F_dx), append(F_sx,F_dx,F).
-
 % example
 % foglie(t(a,t(b,empty,empty),t(e,empty,empty)),X).
+
+foglie(empty, []).
+foglie(t(X, empty, empty), [X]) :- !.
+foglie(t(_, L, R), F) :- foglie(L, F_L), foglie(R, F_R), append(F_L, F_R, F).
