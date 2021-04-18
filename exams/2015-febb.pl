a10cb15041ebe8810a5c83a7038bb22d9586a47e
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-04-18 22:25:05 +0200

    update topic

--- exams/2015-febb.pl
+++ exams/2015-febb.pl
@@ -1,9 +1,9 @@
 % swipl
 % consult('exams/2015-febb.pl').
 
-% Definire un predicato Prolog del first(+X,+L,?Resto), vero se Resto `e la lista che
+% Definire un predicato Prolog del first(+X,+L,?Resto), vero se Resto è la lista che
 % si ottiene da L cancellando la prima occorrenza di X. Fallisce se X non occorre in L.
-% Attenzione: se L contiene pi`u occorrenze di X, il backtracking non deve fornire altre
+% Attenzione: se L contiene più occorrenze di X, il backtracking non deve fornire altre
 % soluzioni (cancellando la seconda occorrenza, la terza ecc.). Usare quindi opportunamente il cut o il not.
 
 del_first(A,[A],[]).
