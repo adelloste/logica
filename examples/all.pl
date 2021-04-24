2f08fc0609c6e34a8fdc8f6e0b48d3b764eeb4dc
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-04-24 19:28:06 +0200

    update all

--- examples/all.pl
+++ examples/all.pl
@@ -66,6 +66,7 @@ rev([X|Rest],Rev) :- rev(Rest,RevRest), append(RevRest,[X],Rev).
 del_first(X,[X|L],L) :- !.
 del_first(X,[Y|L],[Y|Resto]) :- del_first(X,L,Resto).
 
+% Ex 10d
 % del(+X,+L,?Resto) = Resto è la lista che si ottiene da L cancellando tutte le occorrenze di X.
 % Se X non occorre in L, Resto è uguale a L stessa.
 % Attenzione: il backtracking non deve generare altre soluzioni, in cui alcune occorrenze X rimangono nella soluzione.
@@ -73,3 +74,38 @@ del_first(X,[Y|L],[Y|Resto]) :- del_first(X,L,Resto).
 del(_,[],[]).
 del(X,[X|L],Rest) :- !, del(X,L,Rest).
 del(X,[Y|L],[Y|Rest]) :- del(X,L,Rest).
+
+% Ex 10f
+% mkset(+L,-Set) = Set è una lista senza ripetizioni che contiene
+% tutti e solo gli elementi di L (senza utilizzare il predicato predefinito
+% list to set/2).
+
+mkset([],[]).
+mkset([X|Rest],S) :- member(X,Rest), !, mkset(Rest,S).
+mkset([X|Rest],[X|S]) :- mkset(Rest,S).
+
+% Ex 10g
+% union(+A,+B,-Union) = Union è una lista (senza ripetizioni, se
+% anche A e B sono senza ripetizioni) che rappresenta l’unione di A e B.
+union([],B,B).
+union([X|Xs],B,U) :- member(X,B), !, union(Xs,B,U).
+union([X|Xs],B,[X|U]) :- union(Xs,B,U).
+
+% Ex 12
+% Definire un predicato insert(X,L1,L2), vero se L2 si ottiene inserendo X
+% in L1 (in qualsiasi posizione). Almeno una delle due liste L1 e L2 devono
+% essere istanziate. Ad esempio:
+% 
+% ?- insert(a,[1,2],X).
+% X = [a, 1, 2] ;
+% X = [1, a, 2] ;
+% X = [1, 2, a] ;
+% false.
+% 
+% ?- insert(a,X,[1,a,2]).
+% X = [1, 2] ;
+% false.
+
+insert(X,L1,[X|L1]).
+insert(X,[Y|Ys],[Y|N]) :- insert(X,Ys,N).
+
