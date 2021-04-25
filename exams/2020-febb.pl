7669602748c4f1540d3c381133df4d7316311e37
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-04-25 15:34:00 +0200

    add exam 2020-febb

---
+++ exams/2020-febb.pl
@@ -0,0 +1,17 @@
+% swipl
+% consult('exams/2020-febb.pl').
+
+% Definire (senza utilizzare il predicato predefinito select) un predicato
+% Prolog cancella(+X,+L,?Resto), vero se Resto è la lista che si ottiene
+% da L cancellando la prima occorrenza di X. Fallisce se X non occorre in L.
+% Attenzione: se L contiene più occorrenze di X, il backtracking non deve
+% fornire altre soluzioni (cancellando la seconda occconsult('exams/2020-febb.pl').orrenza, la terza ecc.).
+% Usare quindi opportunamente il cut o il not
+
+% example
+% cancella(1,[2,4,5,6,7,1,8,9])
+% assoc(X,[(b,2),(a,1)],V).
+
+cancella(X,[X],[]) :- !.
+cancella(X,[X|Rest],Rest) :- !.
+cancella(X,[Y|Ys],[Y|Rest]) :- cancella(X,Ys,Rest).
