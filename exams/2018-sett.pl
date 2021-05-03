27c320ccfe964f14f18ae2b2447c42f78f41ba99
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-05-03 12:31:06 +0200

    update exam 2018-sett

--- exams/2018-sett.pl
+++ exams/2018-sett.pl
@@ -10,3 +10,11 @@
 foglie(empty, []).
 foglie(t(X, empty, empty), [X]) :- !.
 foglie(t(_, L, R), F) :- foglie(L, F_L), foglie(R, F_R), append(F_L, F_R, F).
+
+% leaves(+Tree,-Foglie,+Leaf)
+% ha successo quando Tree è un albero binario e Foglie è una lista delle foglie di Tree che Leaf come etichetta (in qualsiasi ordine ed eventualmente con ripetizioni).
+
+leaves(empty, []).
+leaves(t(X, empty, empty), [X], X) :- !.
+leaves(t(_, empty, empty), [], _) :- !.
+leaves(t(_, L, R), F, X) :- leaves(L, F_L, X), leaves(R, F_R, X), append(F_L, F_R, F).
